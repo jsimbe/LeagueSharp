a33dcb5584c902a41c3b5b5eee4e0dc32cb5900c
jsimbe <ventocosseus@gmail.com>
2014-10-29 07:08:38 +0800

    Update Lucian.cs

--- Marksman/Lucian.cs
+++ Marksman/Lucian.cs
@@ -62,7 +62,7 @@ namespace Marksman
 
         public bool LucianHasPassive()
         {
-            return DoubleHit;
+            return (ObjectManager.Player.Buffs.All(buff => buff.Name = "lucianpassivebuff"));
         }
 
         public static Obj_AI_Base QMinion
